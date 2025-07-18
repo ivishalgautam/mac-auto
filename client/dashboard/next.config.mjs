929551af245940ed7bac7a1010fb07dab857aabe
Vishal Gautam <ivishalgautamm@gmail.com>
2025-07-18 17:09:53 +0530

    added new pages

--- client/dashboard/next.config.mjs
+++ client/dashboard/next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "localhost",
         port: "3001",
       },
+      {
+        protocol: "https",
+        hostname: "macapi.brandingwaale.com",
+        port: "",
+      },
     ],
   },
 };
