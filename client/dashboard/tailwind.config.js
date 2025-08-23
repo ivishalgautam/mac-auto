8f1022a19b7b9423aee37945ec11c75beaedd9a8
Vishal Gautam <ivishalgautamm@gmail.com>
2025-08-23 13:12:50 +0530

    bug fixes

--- client/dashboard/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-};
