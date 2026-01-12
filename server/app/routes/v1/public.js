03f361a89e26b562c3d680368b9932e8d45f4ed1
Vishal Gautam <ivishalgautamm@gmail.com>
2026-01-12 10:22:35 +0530

    added queries post request

--- server/app/routes/v1/public.js
+++ server/app/routes/v1/public.js
@@ -5,11 +5,16 @@ import { enquiryPublicRoutes } from "../../api/enquiry/routes.js";
 import { walkinEnquiriesPublicRoutes } from "../../api/walkin-enquiry/routes.js";
 import { kylasPublicRoutes } from "../../api/kylas/routes.js";
 import { vehicleColorPublicRoutes } from "../../api/vehicle-color/routes.js";
+import { queryPublicRoutes } from "../../api/query/routes.js";
 
 export default async function routes(fastify, options) {
+  fastify.addHook("preHandler", async (request, reply) => {
+    request.body && console.log("body", request.body);
+  });
   fastify.register(vehiclePublicRoutes, { prefix: "vehicles" });
   fastify.register(financerPublicRoutes, { prefix: "financers" });
   fastify.register(enquiryPublicRoutes, { prefix: "enquiries" });
+  fastify.register(queryPublicRoutes, { prefix: "queries" });
   fastify.register(walkinEnquiriesPublicRoutes, { prefix: "walkin-enquiries" });
   fastify.register(vehicleColorPublicRoutes, { prefix: "vehicle-colors" });
 
